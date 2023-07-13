8c80f00b5876551dbcfe10386ee701643fc60195
jsphjk <josephjakemacabatao@gmail.com>
2023-07-13 23:51:55 +0800

    Build searh input and modify dark mode button

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,16 +1,29 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Icon,
+  IconButton,
+  Switch,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
 import React from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <HStack>
-      <Switch
+      {/* <Switch
         colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
+      /> */}
+      <IconButton
+        aria-label="Dark mode"
+        icon={colorMode == "dark" ? <BsSun /> : <BsMoon />}
+        onClick={toggleColorMode}
       />
-      <Text>Dark Mode</Text>
     </HStack>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack padding="5px" justifyContent={"space-between"}>
+    <HStack padding="5px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
